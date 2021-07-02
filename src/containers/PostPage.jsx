59201771c7abf0bbf9596403f23937a469208a45
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-07-02 23:42:52 +0530

    Convet postPage to functional comp

--- src/containers/PostPage.jsx
+++ src/containers/PostPage.jsx
@@ -1,54 +1,45 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
 import Search from '../components/controls/Search';
 import Results from '../components/display/Results';
 import { Request } from '../services/request-utils';
 
-export default class PostPage extends Component {
-    state = {
-        url: '',
-        method: 'GET',
-        req:'',
-        results: [],
-    }
-    handleSubmit = async (e) => {
-        e.preventDefault();
-        const results = await Request(this.state.method, this.state.url, this.state.req);
-        this.setState({results})
+function PostPage() {
+  const [url, setUrl] = useState('');
+  const [method, setMethod] = useState('GET');
+  const [req, setReq] = useState('');
+  const [results, setResults] = useState([]);
 
- }
-  handleUrlChange = async (e) => {
-      this.setState({url: e.target.value})
-
- }
-  handleMethod = async (e) => {
-       this.setState({method: e.target.value})
-
- }
-  handleJson = async (e) => {
-       this.setState({req: e.target.value})
-
- }
-
-    render() {
-        const {url, method, req, results} =this.state;
-        return (
-            <div>
-                <header>
-                    <h1>Post-MAclone</h1>
-                </header>
-                <Search
-                handleSubmit={this.handleSubmit} 
-                handleUrlChange={this.handleUrlChange}
-                handleMethod={this.handleMethod}
-                handleJson={this.handleJson}
-                url={url} 
-                radioInput={method} 
-                json={req}
-                />
-                <Results results={results}/>
-                
-            </div>
-        )
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const results = await Request(method, url, req);
+    setResults(results);
+  };
+  const handleUrlChange = (e) => {
+    setUrl(e.target.value);
+  };
+  const handleMethod = (e) => {
+    setMethod(e.target.value);
+  };
+  const handleJson = (e) => {
+    setReq(e.target.value);
+  };
+  return (
+    <div>
+      <header>
+        <h1>Post-MAclone</h1>
+      </header>
+      <Search
+        handleSubmit={handleSubmit}
+        handleUrlChange={handleUrlChange}
+        handleMethod={handleMethod}
+        handleJson={handleJson}
+        url={url}
+        radioInput={method}
+        json={req}
+      />
+      <Results results={results} />
+    </div>
+  );
 }
 
+export default PostPage;
