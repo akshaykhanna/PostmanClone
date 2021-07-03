fde10dcc85193f2293df8c1b33fb8cc60c630239
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-07-03 08:09:03 +0530

    Add bootstrap to search page

--- src/components/controls/Search.jsx
+++ src/components/controls/Search.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Form, Col, Row, Button } from 'react-bootstrap';
 
 function Search({
   handleSubmit,
@@ -11,75 +12,91 @@ function Search({
   json,
 }) {
   return (
-    <form name="search-form" onSubmit={handleSubmit}>
-      <input
-        value={url}
-        onInput={handleUrlChange}
-        placeholder="Request Endpoint Here"
-      />
-      <span>
-        <label>
-          <input
+    <Form name="search-form" onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Request Endpoint Here</Form.Label>
+        <Form.Control
+          placeholder="https://www.someUrl.com"
+          value={url}
+          onInput={handleUrlChange}
+        />
+      </Form.Group>
+      <fieldset>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label as="legend" column sm={2}>
+            HTTP verb:
+          </Form.Label>
+          {/* <Col sm={10}> */}
+          <Form.Check
+            column
+            sm={2}
             type="radio"
+            label="GET"
             name="method"
             value="GET"
             checked={radioInput === 'GET'}
             onChange={handleMethod}
+            id="formHorizontalRadios1"
           />
-          <span>Get</span>
-        </label>
-        <label>
-          <input
+          <Form.Check
+            column
+            sm={2}
             type="radio"
             name="method"
             value="POST"
+            label="POST"
             checked={radioInput === 'POST'}
             onChange={handleMethod}
+            id="formHorizontalRadios2"
           />
-          <span>Post</span>
-        </label>
-        <label>
-          <input
+          <Form.Check
+            column
+            sm={2}
             type="radio"
             name="method"
             value="PUT"
+            label="PUT"
             checked={radioInput === 'PUT'}
             onChange={handleMethod}
+            id="formHorizontalRadios3"
           />
-          <span>Put</span>
-        </label>
-        <label>
-          <input
+          <Form.Check
+            column
+            sm={2}
             type="radio"
             name="method"
             value="PATCH"
+            label="PATCH"
             checked={radioInput === 'PATCH'}
             onChange={handleMethod}
+            id="formHorizontalRadios4"
           />
-          <span>Patch</span>
-        </label>
-        <label>
-          <input
+          <Form.Check
+            column
+            sm={2}
             type="radio"
             name="method"
             value="DELETE"
+            label="DELETE"
             checked={radioInput === 'DELETE'}
             onChange={handleMethod}
+            id="formHorizontalRadios4"
           />
-          <span>Delete</span>
-        </label>
-      </span>
-
-      <label>
-        <span>Raw JSON Request</span>
-        <textarea
-          value={json}
-          aria-label="json-request"
-          onInput={handleJson}
-        ></textarea>
-      </label>
-      <button>Submit Request</button>
-    </form>
+          {/* </Col> */}
+        </Form.Group>
+      </fieldset>
+      <Form.Control
+        as="textarea"
+        placeholder="Raw JSON Request"
+        style={{ height: '100px' }}
+        value={json}
+        aria-label="json-request"
+        onInput={handleJson}
+      />
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
 
